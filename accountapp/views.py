b03da41eeeb2704bcfa300a986597ed876dc8aed
do-not-do-that <rl7968@naver.com>
2021-03-23 00:56:25 +0900

    [ Modified ] POST 통신 & 데이터 DB 저장 TEST

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,10 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method=="POST":
-        return render(request, 'accountapp/hello_world.html',context={'text':'POST METHOD'})
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html',context={'hello_world_output':temp})
     else:
         return render(request, 'accountapp/hello_world.html',context={'text':'GET METHOD'})
