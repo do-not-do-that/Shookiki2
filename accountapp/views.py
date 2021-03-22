263bcbc02b092b109ae84456c303962302758e25
do-not-do-that <rl7968@naver.com>
2021-03-23 01:20:55 +0900

    [ Modified ] DB 정보 출력하기

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -13,6 +15,7 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html',context={'hello_world_output':temp})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html',context={'text':'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})
