d36f200bccbe5c0d42f405ceb762d2a06409ec56
do-not-do-that <rl7968@naver.com>
2021-03-24 01:51:14 +0900

    [ Create ] accountapp/templates/accountapp/create.html [ Modified ] accountapp/urls.py , accountapp/views.py

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 
 
 urlpatterns = [
-    path('hello_world/',hello_world, name='hello_world')
+    path('hello_world/',hello_world, name='hello_world'),
+    path('create/',AccountCreateView.as_view(), name='create')
 ]
