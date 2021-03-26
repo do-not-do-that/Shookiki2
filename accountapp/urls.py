299a8d5543313962fefb8ec6eeafc1f4fc5380a1
do-not-do-that <rl7968@naver.com>
2021-03-26 15:40:54 +0900

    [ Create ] detail.html [ Modified ] accountapp/urls.py, accountapp/views.py (DetailView 를 이용해 개인 페이지 구현하기)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = 'accountapp'
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/',LogoutView.as_view(), name='logout'),
 
-    path('create/',AccountCreateView.as_view(), name='create')
+    path('create/',AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>',AccountDetailView.as_view(), name='detail'),
 ]
