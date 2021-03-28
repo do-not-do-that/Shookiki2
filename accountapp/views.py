b81a9787f1b3886254d38448124326c11af0a0fd
do-not-do-that <rl7968@naver.com>
2021-03-28 11:41:36 +0900

    [ Create ] forms.py, update.html [ Modified ] accountapp/detail.html, accountapp/ursl.py, accountapp/views.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import HelloWorld
+from accountapp.templates.accountapp.forms import AccountUpdateForm
 
 
 def hello_world(request):
@@ -35,3 +36,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
