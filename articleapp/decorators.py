7ef64a5e8b5b67ab43694135e5cee64ffcafa895
do-not-do-that <rl7968@naver.com>
2021-04-02 15:55:13 +0900

    [ CRUD ] Article App CRUD 구현

---
+++ articleapp/decorators.py
@@ -0,0 +1,16 @@
+
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+
+
+def article_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+
+        if not article.writer== request.user:
+            return HttpResponseForbidden()
+
+        return func(request,*args, **kwargs)
+
+    return decorated
