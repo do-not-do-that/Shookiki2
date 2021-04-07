e66f397f8cabcce4a767d67a5ffa6c80e39dbb95
do-not-do-that <rl7968@naver.com>
2021-04-08 06:31:39 +0900

    WYSIWYG 적용(create, update 전부)

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height:auto;text-align:left;'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title','image','project','content']
