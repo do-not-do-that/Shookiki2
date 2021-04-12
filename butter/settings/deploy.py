ca3d8bffd56682615ce7e02405e7ed30c7a515de
do-not-do-that <rl7968@naver.com>
2021-04-12 20:32:59 +0900

    docker secret

--- butter/settings/deploy.py
+++ butter/settings/deploy.py
@@ -1,4 +1,12 @@
 from .base import *
+def read_secret(secret_name):
+
+    file = open('/run/secrets/'+secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
 
 env = environ.Env(
     # set casting, default value
@@ -14,7 +22,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,7 +37,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'wjdqhqhdks',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
